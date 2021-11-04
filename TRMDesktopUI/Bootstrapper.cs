1367812f30c12d5cf524b73ea6e5df2394af8aae
oleksiy01234 <45813923+oleksiy01234@users.noreply.github.com>
2021-11-04 11:11:41 -0700

    Added IoC (inversion of control) container DI (dep inj)

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -1,4 +1,7 @@
 ﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using TRMDesktopUI.ViewModels;
 
@@ -6,14 +9,50 @@ namespace TRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private readonly SimpleContainer container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            container.Instance(container);
+
+            // 2 things that calibermicro wants
+            container
+                .Singleton<IWindowManager, WindowManager>() // bringing windows in and out
+                .Singleton<IEventAggregator, EventAggregator>(); // event messaging
+
+            // reflection: powerful but slow. Configure is run once so it's fine
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            // get the instance from container instead (dep inj)
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
         }
     }
 }
