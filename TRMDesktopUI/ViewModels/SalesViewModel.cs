f993f2b90541a3bb6e137764cfb1f1bbb2799075
oleksiy01234 <45813923+oleksiy01234@users.noreply.github.com>
2021-11-09 14:36:06 -0800

    Added Sales Page
    Added the sales page xaml and supporting view model. This scaffolds out the VM, without adding actual logic.

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Caliburn.Micro;
+using System.ComponentModel;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get => _products;
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get => _cart;
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get => _itemQuantity;
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public string SubTotal => "$0.00";
+        public string Tax => "$0.00";
+        public string Total => "$0.00";
+
+        public bool CanAddToCart => false;
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart => false;
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut => false;
+
+        public void CheckOut()
+        {
+
+        }
+    }
+}
---
+++ TRMDesktopUI/Views/SalesView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TRMDesktopUI.Views
+{
+    /// <summary>
+    /// Interaction logic for SalesView.xaml
+    /// </summary>
+    public partial class SalesView : UserControl
+    {
+        public SalesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
