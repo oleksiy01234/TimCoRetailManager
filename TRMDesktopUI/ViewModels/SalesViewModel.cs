fbcdd8fe671a0bfe9226dcf76592605a16f8a236
oleksiy01234 <45813923+oleksiy01234@users.noreply.github.com>
2021-12-01 11:30:54 -0800

    Changed ItemQuantity from string to int

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -29,9 +29,9 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
-        private string _itemQuantity;
+        private int _itemQuantity;
 
-        public string ItemQuantity
+        public int ItemQuantity
         {
             get => _itemQuantity;
             set
